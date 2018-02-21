9924ceff39e0dcaa2a1b9327795f12de9f6fb3a8
Jamie Lord <jamie@lord.technology>
2018-02-21 14:20:29 +0000

    Moved to bootstrap 4 and added root comment list to index

--- SocketComment/Pages/Index.cshtml.cs
+++ SocketComment/Pages/Index.cshtml.cs
@@ -4,14 +4,32 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MyCouch;
+using SocketComment.Models;
 
 namespace SocketComment.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+        [BindProperty]
+        public List<Comment> RootComments { get; set; }
 
+        public async Task<IActionResult> OnGet()
+        {
+            using (var store = new MyCouchStore("http://localhost:5984", "comments"))
+            {
+                var query = new Query("comments", "all_roots");
+
+                var r = await store.QueryAsync<Comment>(query);
+
+                RootComments = new List<Comment>();
+                foreach (var row in r)
+                {
+                    RootComments.Add(row.Value);
+                }
+            }
+
+            return Page();
         }
     }
 }
